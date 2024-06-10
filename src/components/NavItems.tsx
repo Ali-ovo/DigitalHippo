bc92bea7c3bf116ddfe80d9ebc20a97267b7bb92
ali <ali_ovo@qq.com>
2024-06-10 13:54:58 +0800

    feat: create navbar component

---
+++ src/components/NavItems.tsx
@@ -0,0 +1,59 @@
+'use client'
+
+import { PRODUCT_CATEGORIES } from '@/config'
+import { useEffect, useRef, useState } from 'react'
+import NavItem from './NavItem'
+import { useClickAway } from 'ahooks'
+
+const NavItems = () => {
+  const [activeIndex, setActiveIndex] = useState<null | number>()
+
+  const isAnyOpen = activeIndex !== null
+
+  const navRef = useRef<HTMLDivElement | null>(null)
+
+  useClickAway(() => {
+    setActiveIndex(null)
+  }, navRef)
+
+  useEffect(() => {
+    const handler = (e: KeyboardEvent) => {
+      if (e.key === 'Escape') {
+        setActiveIndex(null)
+      }
+    }
+
+    document.addEventListener('keydown', handler)
+
+    return () => {
+      document.removeEventListener('keydown', handler)
+    }
+  }, [])
+
+  return (
+    <div className='flex gap-4 h-full' ref={navRef}>
+      {PRODUCT_CATEGORIES.map((category, i) => {
+        const handleOpen = () => {
+          if (activeIndex === i) {
+            setActiveIndex(null)
+          } else {
+            setActiveIndex(i)
+          }
+        }
+        const isOpen = activeIndex === i
+
+        return (
+          <NavItem
+            category={category}
+            handleOpen={handleOpen}
+            isOpen={isOpen}
+            key={category.value}
+            isAnyOpen={isAnyOpen}
+          />
+        )
+      })}
+    </div>
+  )
+}
+
+export default NavItems
